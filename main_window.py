9ad96e3b6023b352daaf8cd4520567214e8335f4
Davy Oliveira <contato_davyluiz@hotmail.com>
2023-08-03 09:16:41 -0300

    Adicionando tela principal.

---
+++ main_window.py
@@ -0,0 +1,16 @@
+from PySide2 import QtCore, QtGui, QtWidgets
+from base_window import BaseWindow
+
+
+class MainWindow(BaseWindow):
+    def __init__(self):
+        super().__init__()
+
+    def setup_header(self, layout):
+        pass
+
+    def setup_search_field(self, layout):
+        pass
+
+    def setup_explanation_text(self, layout):
+        pass
